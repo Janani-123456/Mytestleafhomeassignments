bee5c5df690ea4fd3e2d00f95204abc232a233e9
Janani-123456 <rjanani29@gmail.com>
2025-07-20 13:01:43 +0530

    Add files via upload
    Day 1

---
+++ day1/Chrome.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortCutKey = 'C';
+		System.out.println("The Version is : " + version);
+		System.out.println("The developer is : " + developer);
+		System.out.println("The Boolean value isBeta is : " + isBeta);
+		System.out.println("The releaseYear is : " + releaseYear);
+		System.out.println("The shortCutKey is : " + shortCutKey);
+	}
+
+}
